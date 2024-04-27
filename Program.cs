bc036e989d0e76d3d33b6d04d0d222c1b0fcd87b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-26 21:26:01 -0300

    Exercício Realizado

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int glicemia;
+
+Console.Write("Digite a sua glicemia (em mg/dL): ");
+glicemia = Convert.ToInt32(Console.ReadLine());
+
+if(glicemia > 126)
+{
+    ﻿Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Diabetes Mellitus na Gravidez"); 
+}
+else
+{
+    if(glicemia >= 92)
+    {
+        ﻿Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Compatível com Diabetes Gestacional");
+    }
+    else
+    {
+        ﻿Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
+    }
+}
+
+Console.WriteLine();
+
+
+Console.ResetColor();
